ffe7c9bb5a64bf78678abfb1fc0de935d1c6e6a9
srijan <srijan.dhl21@gmail.com>
2024-01-05 18:25:40 +1100

    .

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { getProductById, getProducts } from '../model/product/productModel.js';
+import { getProductById, getProducts, getProductsByCategory } from '../model/product/productModel.js';
 
 const router = express.Router();
 
@@ -16,6 +16,20 @@ router.get(':_id?', async (req: Request, res: Response, next: NextFunction) => {
         });
     } catch (error) {
         next(error);
+    }
+});
+router.get('/category/:_id?', async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const { _id } = req.params;
+        const products = await getProductsByCategory(_id);
+        res.json({
+            status: "success",
+            message: "Here are the product/s by category",
+            products,
+        });
+    } catch (error) {
+
     }
 });
 export default router;
