f67b4dc02f6a01e939f83540be6d4f0101666651
srijan <srijan.dhl21@gmail.com>
2024-03-08 13:01:25 +1100

    aws_api_issue_7

--- build/helper/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-const salt = 10;
-export const hashPassword = (plainPass) => {
-    return bcrypt.hashSync(plainPass, salt);
-};
-export const comparePassword = (plainPass, hashPass) => {
-    return bcrypt.compareSync(plainPass, hashPass);
-};
-//# sourceMappingURL=bcrypt.js.map
