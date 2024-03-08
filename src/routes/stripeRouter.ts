987b7a30d83acd2fbe9b442e9c0129976be783d8
srijan <srijan.dhl21@gmail.com>
2024-03-08 13:17:04 +1100

    aws_api_issue_9

--- src/routes/stripeRouter.ts
+++ src/routes/stripeRouter.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 
 const router = express.Router();
 
-
+console.log(process.env.STRIPE_SECRET_KEY);
 if (!process.env.STRIPE_SECRET_KEY) {
     throw new Error('Stripe secret key is not defined in environment variables');
 }
